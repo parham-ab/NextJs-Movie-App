accc183b3b803e94a3116a7e6eb51300dd4c1860
Parham Abolghasemi <parhamab17@gmail.com>
2022-04-09 14:43:14 +0430

    installing initial packages, folder structure updating

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+// components
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
