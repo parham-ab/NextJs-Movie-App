784d6fc772aca8e6643448425cf2948fb23b208b
Parham Abolghasemi <parhamab17@gmail.com>
2022-09-03 17:31:00 +0430

    added

--- components/common/hooks/UseHead.js
+++ components/common/hooks/UseHead.js
@@ -4,6 +4,7 @@ const UseHead = ({ title, description, keywords }) => {
   return (
     <Head>
       <title>{title}</title>
+      <link rel="icon" type="image/x-icon" href="/img/movieMan.svg" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
     </Head>
